7032f7f78f2daece06e8085aee39023f089b8d6a
Hermosilla Reinaldo <ghostcad@gmail.com>
2020-06-22 20:22:21 -0300

    Creados dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CrearPersonaComponent } from './crear-persona/crear-persona.component';
+import { ConsultarPersonasComponent } from './consultar-personas/consultar-personas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearPersonaComponent,
+    ConsultarPersonasComponent
   ],
   imports: [
     BrowserModule

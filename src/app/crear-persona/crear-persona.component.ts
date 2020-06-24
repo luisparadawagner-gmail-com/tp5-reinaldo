c0fce51d980e6d1cfd4b10d1b6ebec2328a0d0b0
Hermosilla Reinaldo <ghostcad@gmail.com>
2020-06-24 16:41:18 -0300

    Agregada funcionalidad basica de formulario crear persona

--- src/app/crear-persona/crear-persona.component.ts
+++ src/app/crear-persona/crear-persona.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Persona } from '../clases/persona';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-crear-persona',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearPersonaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private route: ActivatedRoute) {}   
 
-  ngOnInit(): void {
+  param: any;
+  personaForm: FormGroup;
+
+ngOnInit() {
+    debugger;
+
+    this.param = this.route.snapshot.params;
+
+    if (Object.keys(this.param).length) {
+			this.iniciarFormulario(this.param);
+		} else {
+			this.defaultFormulario();
+		}
   }
+
+    iniciarFormulario(personaImp : Persona){
+      this.personaForm = this.fb.group({
+        nombre: [ personaImp.nombre, Validators.required ],
+        apellido: [ personaImp.apellido ],
+        edad: [ personaImp.edad ],
+        direccion: [ personaImp.direccion ]
+      });
+    }
+
+    defaultFormulario(){
+      this.personaForm = this.fb.group({
+        nombre: [ 'vacio', Validators.required ],
+        apellido: [ '' ],
+        edad: [ '' ],
+        direccion: [ '' ]
+      });
+    }
 
 }
